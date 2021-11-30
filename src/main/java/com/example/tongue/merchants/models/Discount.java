249b577528022dc10e7b7ced5fd792cf1a8ef778
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2021-11-30 15:48:01 -0500

    Discount Unit Tests

--- src/main/java/com/example/tongue/merchants/models/Discount.java
+++ src/main/java/com/example/tongue/merchants/models/Discount.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -333,6 +334,13 @@ public class Discount {
             }
         }
         return true;
+    }
+
+    @JsonIgnore
+    public void addEntitledProduct(Product product){
+        if (this.entitledProducts==null)
+            this.entitledProducts = new ArrayList<>();
+        this.entitledProducts.add(product);
     }
 
     @JsonIgnore
---
+++ src/test/java/com/example/tongue/merchants/models/DiscountUnitTest.java
@@ -0,0 +1,138 @@
+package com.example.tongue.merchants.models;
+
+import com.example.tongue.merchants.enumerations.DiscountScope;
+import com.example.tongue.merchants.enumerations.DiscountType;
+import com.example.tongue.merchants.enumerations.ProductsScope;
+import com.example.tongue.shopping.models.Cart;
+import com.example.tongue.shopping.models.LineItem;
+import com.example.tongue.shopping.models.LineItemPrice;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class DiscountUnitTest {
+
+    @Test
+    public void givenNoEntitledProductAndGlobalScopeWhenValidatingProductThenTrue(){
+        Product product = new Product();
+        Discount discount = new Discount();
+        discount.setProductsScope(ProductsScope.ALL);
+        assertTrue(discount.validForProduct(product));
+    }
+
+    @Test
+    public void givenEntitledProductAndGlobalScopeWhenValidatingProductThenTrue(){
+        Product product1 = new Product();
+        product1.setId(1L);
+        Product product2 = new Product();
+        product2.setId(2L);
+        Discount discount = new Discount();
+        discount.setProductsScope(ProductsScope.ALL);
+        discount.addEntitledProduct(product2);
+        assertTrue(discount.validForProduct(product1));
+    }
+
+    @Test
+    public void givenEntitledProductAndEntitledScopeWhenValidatingProductThenFalse(){
+        Product product1 = new Product();
+        product1.setId(1L);
+        Product product2 = new Product();
+        product2.setId(2L);
+        Discount discount = new Discount();
+        discount.setProductsScope(ProductsScope.ENTITLED_ONLY);
+        discount.addEntitledProduct(product2);
+        assertFalse(discount.validForProduct(product1));
+    }
+
+    @Test
+    public void givenEntitledProductAndEntitledScopeWhenValidatingProductThenTrue(){
+        Product product1 = new Product();
+        product1.setId(1L);
+        Product product2 = new Product();
+        product2.setId(1L);
+        Discount discount = new Discount();
+        discount.setProductsScope(ProductsScope.ENTITLED_ONLY);
+        discount.addEntitledProduct(product2);
+        assertTrue(discount.validForProduct(product1));
+    }
+
+    @Test
+    public void givenLineItemPriceConditionAndEntitledScopeWhenValidatingProductThenTrue(){
+        Product product1 = new Product();
+        product1.setId(1L);
+        product1.setPrice(BigDecimal.valueOf(200));
+        Product product2 = new Product();
+        product2.setId(1L);
+        Discount discount = new Discount();
+        discount.setProductsScope(ProductsScope.ENTITLED_ONLY);
+        discount.addEntitledProduct(product2);
+        LineItemPriceCondition itemPriceCondition = new LineItemPriceCondition();
+        itemPriceCondition.setHeq(BigDecimal.valueOf(100));
+        discount.setLineItemPriceCondition(itemPriceCondition);
+        assertTrue(discount.validForProduct(product1));
+    }
+
+    @Test
+    public void givenItemPriceConditionEntitledScopeAndNoProductPriceWhenValidatingProductThenFalse(){
+        Product product1 = new Product();
+        product1.setId(1L);
+        Product product2 = new Product();
+        product2.setId(1L);
+        Discount discount = new Discount();
+        discount.setProductsScope(ProductsScope.ENTITLED_ONLY);
+        discount.addEntitledProduct(product2);
+        LineItemPriceCondition itemPriceCondition = new LineItemPriceCondition();
+        itemPriceCondition.setHeq(BigDecimal.valueOf(100));
+        discount.setLineItemPriceCondition(itemPriceCondition);
+        assertFalse(discount.validForProduct(product1));
+    }
+
+    @Test
+    public void shouldValidForCartReturnTrueWhenProductNotEntitled(){
+        /** To validate**/
+        Cart cart = new Cart();
+        LineItem item = new LineItem();
+        Product product = new Product(); product.setId(2L); product.setPrice(BigDecimal.ONE);
+        item.setProduct(product); item.getPrice().setFinalPrice(product.getPrice());
+        cart.addItem(item);
+        /** Discount instantiation**/
+        Product product1 = new Product();
+        product1.setId(1L);
+        product1.setPrice(BigDecimal.valueOf(20.0));
+        Discount discount = new Discount();
+        discount.setProductsScope(ProductsScope.ENTITLED_ONLY);
+        discount.setEntitledProducts(Arrays.asList(product1));
+        Boolean current = discount.validForCart(cart);
+        assertTrue(current);
+    }
+
+    @Test
+    public void givenNoEntitledCartItemsWhenDiscountProductScopeIsGlobalAndNoEmptyEntitledThenOk(){
+
+        Cart cart = new Cart();
+
+        LineItem item1 = new LineItem(); item1.setQuantity(2);
+        Product product1 = new Product(); product1.setId(1L);
+        item1.setProduct(product1);
+
+        cart.addItem(item1);
+
+        Product product2 = new Product(); product2.setId(2L);
+        Product product3 = new Product(); product3.setId(3L);
+
+        Discount discount = new Discount();
+        discount.addEntitledProduct(product2);
+        discount.addEntitledProduct(product3);
+        discount.setProductsScope(ProductsScope.ALL);
+        discount.setDiscountScope(DiscountScope.LINE_ITEMS);
+        discount.setDiscountType(DiscountType.PRODUCT);
+
+        assertTrue(discount.validForCart(cart));
+
+
+    }
+
+}
