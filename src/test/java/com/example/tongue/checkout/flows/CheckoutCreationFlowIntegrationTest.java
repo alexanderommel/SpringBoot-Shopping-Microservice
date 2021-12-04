6f9f00e5d444e1f0c574b8bfb9bf080da96eef7e
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2021-12-03 20:03:52 -0500

    Create CheckoutCreationFlowIntegrationTest

---
+++ src/test/java/com/example/tongue/checkout/flows/CheckoutCreationFlowIntegrationTest.java
@@ -0,0 +1,169 @@
+package com.example.tongue.checkout.flows;
+
+import com.example.tongue.checkout.models.Checkout;
+import com.example.tongue.checkout.models.FlowMessage;
+import com.example.tongue.core.genericdata.Store1DataGenerator;
+import com.example.tongue.locations.models.Location;
+import com.example.tongue.locations.repositories.LocationRepository;
+import com.example.tongue.merchants.models.*;
+import com.example.tongue.merchants.repositories.*;
+import com.example.tongue.shopping.models.Cart;
+import com.example.tongue.shopping.models.LineItem;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CheckoutCreationFlowIntegrationTest {
+
+    private CheckoutCreationFlow checkoutCreationFlow;
+    //
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    StoreRepository storeRepository;
+    @Autowired
+    MerchantRepository merchantRepository;
+    @Autowired
+    StoreVariantRepository storeVariantRepository;
+    @Autowired
+    DiscountRepository discountRepository;
+    @Autowired
+    ProductImageRepository imageRepository;
+    @Autowired
+    LocationRepository locationRepository;
+    @Autowired
+    GroupModifierRepository groupModifierRepository;
+    @Autowired
+    ModifierRepository modifierRepository;
+    @Autowired
+    CollectionRepository collectionRepository;
+    @Autowired
+    CollectionProductAllocationRepository
+    collectionProductAllocationRepository;
+    @Autowired
+    HttpSession httpSession;
+    @Autowired
+    CheckoutValidation checkoutValidation;
+    @Autowired
+    CheckoutSession checkoutSession;
+
+    //
+
+    @BeforeAll
+    public void setUp(){
+        /** Generate data for testing **/
+        Store1DataGenerator.getInstance( productRepository,
+                 storeRepository,
+                 merchantRepository,
+                 storeVariantRepository,
+                 discountRepository,
+                 imageRepository,
+                 locationRepository,
+                 groupModifierRepository,
+                 modifierRepository,
+                 collectionRepository,
+                collectionProductAllocationRepository);
+        /** **/
+    }
+
+    private void describeGeneratedData(){
+        Long storeVariantId = storeVariantRepository.findAll().get(0).getId();
+        System.out.println("Store Variant Id: "+storeVariantId);
+        List<Collection> collections =
+                collectionRepository.findAllByStoreVariantId(storeVariantId,null ).getContent();
+        for (Collection c:collections
+        ) {
+            Long collectionId = c.getId();
+            System.out.println("Collection id: "+collectionId);
+            System.out.println("Collection title: "+c.getTitle());
+            List<CollectionProductAllocation> allocations =
+                    collectionProductAllocationRepository.findAllByCollection_Id(collectionId,null).getContent();
+            List<Long> ids = new ArrayList<>();
+            for (CollectionProductAllocation a:allocations
+                 ) {
+                Long productId = a.getProduct().getId();
+                ids.add(productId);
+            }
+            System.out.println("Product ids: "+ids.toString());
+        }
+    }
+
+    @Before
+    public void before(){
+        checkoutCreationFlow = new CheckoutCreationFlow(checkoutValidation,checkoutSession);
+        System.out.println("HTTP SESSION ID: "+httpSession.getId());
+    }
+
+    @Test
+    public void showDataInformation(){
+        System.out.println("---Data generated info---");
+        describeGeneratedData();
+        System.out.println("\n");
+    }
+
+    @Test
+    public void shouldReturnTrueGivenThatCheckoutEntryIsValid(){
+        /** Input **/
+        Checkout checkout = new Checkout();
+        checkout = createBasicValidCheckout();
+        /** **/
+        FlowMessage flowMessage = checkoutCreationFlow.run(checkout,httpSession);
+        assertTrue(flowMessage.isSolved());
+    }
+
+    @Test
+    public void givenCheckoutWithoutOriginWhenCreatingThenReturnValidationErrorStage(){
+        String expected = "Validation error";
+        Checkout checkout = createBasicValidCheckout();
+        checkout.setOrigin(null);
+        FlowMessage flowMessage = checkoutCreationFlow.run(checkout,httpSession);
+        assertEquals(flowMessage.getErrorStage(),expected);
+
+    }
+
+    @Test
+    public void givenCheckoutWithNoExistingProductIdWhenCreatingThenReturnErrorMessage(){
+        String expected = "No such Product with id '9994'";
+        Checkout checkout = createBasicValidCheckout();
+        checkout.getCart().getItems().get(0).getProduct().setId(9994L);
+        FlowMessage flowMessage = checkoutCreationFlow.run(checkout,httpSession);
+        assertEquals(expected,flowMessage.getErrorMessage());
+    }
+
+    private Checkout createBasicValidCheckout(){
+        Checkout checkout = new Checkout();
+        Cart cart = new Cart();
+        cart.setInstructions("McLoving");
+        LineItem item= new LineItem();
+        item.setQuantity(2);
+        Product product = new Product();
+        product.setId(372L);
+        Location destination = new Location();
+        destination.setLatitude(22.2F); destination.setLongitude(44.0F);
+        Location origin = new Location();
+        origin.setLatitude(10.0F); origin.setLongitude(5.552222F);
+        StoreVariant storeVariant = new StoreVariant();
+        storeVariant.setId(4L);
+        item.setProduct(product);
+        cart.addItem(item);
+        checkout.setCart(cart);
+        checkout.setOrigin(origin);
+        checkout.setDestination(destination);
+        checkout.setStoreVariant(storeVariant);
+        return checkout;
+    }
+
+}
