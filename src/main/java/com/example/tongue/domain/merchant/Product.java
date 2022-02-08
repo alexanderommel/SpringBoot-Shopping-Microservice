2df4c7ed181b097e04c21a788b38079d86ab9a5e
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-02-07 19:53:50 -0500

    Added ManyToOne relation between products and collections

--- src/main/java/com/example/tongue/domain/merchant/Product.java
+++ src/main/java/com/example/tongue/domain/merchant/Product.java
@@ -3,19 +3,30 @@ package com.example.tongue.domain.merchant;
 
 import com.example.tongue.domain.merchant.enumerations.ProductStatus;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.*;
 import java.math.BigDecimal;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Product {
 
     //FIELDS
-    private @Id @GeneratedValue Long id;
+    private @Id
+    @GeneratedValue
+    Long id;
 
 
-    @Size(max=200,
+    @Size(max = 200,
             message = "Description must be between 0 and 200 characters")
     private String description;
 
@@ -23,7 +34,7 @@ public class Product {
     @Size(max = 100, message = "Handle must be between 0 and 100 characters")
     @Pattern(regexp = "[a-zA-z0-9]{1,50}",
             message = "Pattern must contain only numbers and letters," +
-            " also its size must be between 1 and 50")
+                    " also its size must be between 1 and 50")
     private String handle;
 
 
@@ -36,15 +47,14 @@ public class Product {
             message = "Type must be between 1 a 50 characters")
     private String type;
 
-    private ProductStatus status=ProductStatus.ACTIVE; //active|draft|archived
+    private ProductStatus status = ProductStatus.ACTIVE; //active|draft|archived
 
 
     @Pattern(regexp = "([a-zA-Z0-9]{1,20},){0,10}[a-zA-Z0-9]{1,20}",
             message =
-    "Tags must follow the next regular expression" +
-            " ([a-zA-Z0-9]{1,20},){0,10}[a-zA-Z0-9]{1,20}")
+                    "Tags must follow the next regular expression" +
+                            " ([a-zA-Z0-9]{1,20},){0,10}[a-zA-Z0-9]{1,20}")
     private String tags;
-
 
 
     //@NotBlank(message = "Store must not be empty")
@@ -60,13 +70,13 @@ public class Product {
     @NotNull(message = "Null value not admitted for Price")
     @Min(value = 0, message = "Price should be higher than zero")
     @Max(value = 1000, message = "Price must not be higher than 1000$")
-    private BigDecimal price=BigDecimal.ZERO;
+    private BigDecimal price = BigDecimal.ZERO;
 
 
     @NotEmpty(message = "Currency code must not be empty")
     @Pattern(regexp = "(USD)") //ISO 4217
     @Column(name = "currency_code", updatable = false)
-    private String currency_code="USD";
+    private String currency_code = "USD";
 
 
     @Column(name = "original_price")
@@ -75,6 +85,8 @@ public class Product {
 
     private String adjustments; // {price:25.50,date:25-06-2021}
 
+    @ManyToOne
+    private Collection collection;
 
 
     //METHODS
@@ -83,6 +95,7 @@ public class Product {
         return storeVariant;
     }
 
+    /*
     public void setStoreVariant(StoreVariant storeVariant) {
         this.storeVariant = storeVariant;
     }
@@ -183,7 +196,7 @@ public class Product {
         this.adjustments = adjustments;
     }
 
-
+     */
 
     @Override
     public boolean equals(Object obj) {
