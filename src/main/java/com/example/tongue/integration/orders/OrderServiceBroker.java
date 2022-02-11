2974cf2ed4b391eb85a6a006c50a208465407ef4
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-02-11 12:09:18 -0500

    view restaurants use case

---
+++ src/main/java/com/example/tongue/integration/orders/OrderServiceBroker.java
@@ -0,0 +1,27 @@
+package com.example.tongue.integration.orders;
+
+import com.example.tongue.core.domain.Position;
+import com.example.tongue.domain.merchant.StoreVariant;
+import com.example.tongue.repositories.merchant.StoreVariantRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class OrderServiceBroker {
+
+    private StoreVariantRepository storeVariantRepository;
+
+    public OrderServiceBroker(@Autowired StoreVariantRepository storeVariantRepository){
+        this.storeVariantRepository=storeVariantRepository;
+    }
+
+    public List<StoreVariant> getNearestRestaurants(Position position){
+        log.info("Searching restaurants near to {"+position+"}");
+        // Http Call to Order Service to get active restaurants
+        return  storeVariantRepository.findAll();
+    }
+}
--- src/main/java/com/example/tongue/resources/merchant/StoreVariantRestController.java
+++ src/main/java/com/example/tongue/resources/merchant/StoreVariantRestController.java
@@ -1,27 +1,84 @@
 package com.example.tongue.resources.merchant;
 
+import com.example.tongue.core.domain.Position;
 import com.example.tongue.core.utilities.RestControllerRoutines;
+import com.example.tongue.domain.merchant.Collection;
+import com.example.tongue.domain.merchant.Discount;
 import com.example.tongue.domain.merchant.enumerations.StoreVariantType;
 import com.example.tongue.domain.merchant.StoreVariant;
+import com.example.tongue.integration.orders.OrderServiceBroker;
+import com.example.tongue.integration.shipping.ShippingBrokerResponse;
+import com.example.tongue.integration.shipping.ShippingServiceBroker;
+import com.example.tongue.integration.shipping.ShippingSummary;
+import com.example.tongue.repositories.merchant.CollectionRepository;
+import com.example.tongue.repositories.merchant.DiscountRepository;
 import com.example.tongue.repositories.merchant.StoreVariantRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import java.util.*;
 
+@Slf4j
 @RestController
 public class StoreVariantRestController {
 
     private StoreVariantRepository storeVariantRepository;
-    public StoreVariantRestController(@Autowired StoreVariantRepository storeVariantRepository){
+    private OrderServiceBroker orderServiceBroker;
+    private ShippingServiceBroker shippingServiceBroker;
+    private CollectionRepository collectionRepository;
+    private DiscountRepository discountRepository;
+
+    public StoreVariantRestController(@Autowired StoreVariantRepository storeVariantRepository,
+                                      @Autowired OrderServiceBroker orderServiceBroker,
+                                      @Autowired ShippingServiceBroker shippingServiceBroker,
+                                      @Autowired CollectionRepository collectionRepository,
+                                      @Autowired DiscountRepository discountRepository){
+
         this.storeVariantRepository= storeVariantRepository;
+        this.orderServiceBroker=orderServiceBroker;
+        this.shippingServiceBroker=shippingServiceBroker;
+        this.collectionRepository=collectionRepository;
+        this.discountRepository=discountRepository;
     }
+
+
+    @GetMapping("/stores")
+    public ResponseEntity<Map<String,Object>> getNearestRestaurants(Position position){
+        log.info("Retrieving restaurants by condition 'nearest'");
+        Map<String, Object> response = new HashMap<>();
+        List<StoreVariant> storeVariantList = orderServiceBroker.getNearestRestaurants(position);
+        List<ShippingSummary> shippingSummaries = new ArrayList<>();
+        if (storeVariantList==null){
+            log.info("No restaurants found for position {"+position+"}");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        for (StoreVariant s:storeVariantList) {
+            Position destination = s.getLocation();
+            ShippingBrokerResponse brokerResponse =
+                    shippingServiceBroker.requestShippingSummary(position,destination);
+            if (brokerResponse.getSolved()){
+                shippingSummaries.add((ShippingSummary) brokerResponse.getMessage("summary"));
+                continue;
+            }
+            log.info("Shipping Summary query failed for store_variant with id: "+s.getId());
+            shippingSummaries.add(null);
+        }
+        response.put("stores",storeVariantList);
+        response.put("shipping_summaries",shippingSummaries);
+        log.info("Request status OK");
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    
 
     @GetMapping(value = "/storevariants",params = {"page","size"})
     public ResponseEntity<Map<String,Object>> all(@RequestParam(defaultValue = "0") int page
