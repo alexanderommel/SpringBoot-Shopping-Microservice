7db32ca9e4379e6d4fd8a241c8bc6d4ea5ace2e7
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-03-23 23:57:04 -0500

    Order Accepted (Fulfillment use case) - Repositories

--- src/main/java/com/example/tongue/repositories/FulfillmentRepository.java
+++ src/main/java/com/example/tongue/repositories/FulfillmentRepository.java
@@ -3,5 +3,8 @@ package com.example.tongue.repositories;
 import com.example.tongue.domain.checkout.Fulfillment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FulfillmentRepository extends JpaRepository<Fulfillment, Long> {
+    Optional<Fulfillment> findByCheckoutId(Long id);
 }
