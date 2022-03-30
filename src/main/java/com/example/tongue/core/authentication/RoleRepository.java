734aabb42345c77ab4aeb84d2525b535cae57de1
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-03-30 17:26:11 -0500

    Deleted some classes since they are not used anymore

--- src/main/java/com/example/tongue/core/authentication/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tongue.core.authentication;
-
-import com.example.tongue.core.authentication.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Integer> {
-
-}
