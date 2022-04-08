878bf3fa08cc6c538664cdea22236a13f81797a3
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-04-07 23:54:55 -0500

    Security config and profiles

---
+++ src/main/java/com/example/tongue/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.tongue.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private String customerServiceKey;
+    private String serviceName;
+    private CustomerAuthenticationManager customerAuthenticationManager;
+
+    public SecurityConfig(@Autowired CustomerAuthenticationManager authenticationManager,
+                          @Value("${customer.management.service.key}") String customerServiceKey,
+                          @Value("${shopping.service.name}") String serviceName){
+        this.customerAuthenticationManager=authenticationManager;
+        this.customerServiceKey=customerServiceKey;
+        this.serviceName=serviceName;
+    }
+
+    public CustomerJWTAuthenticationFilter customerJwtFilter(){
+        CustomerJWTAuthenticationFilter filter = new CustomerJWTAuthenticationFilter(
+                customerAuthenticationManager,
+                customerServiceKey,
+                serviceName);
+        return filter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/stores/**").permitAll()
+                .antMatchers("/collections/**").permitAll()
+                .antMatchers("/products/**").permitAll()
+                .antMatchers("/dev/customers/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(customerJwtFilter(), UsernamePasswordAuthenticationFilter.class)
+                .formLogin().disable()
+                .httpBasic().disable()
+                .csrf().disable();
+    }
+}
