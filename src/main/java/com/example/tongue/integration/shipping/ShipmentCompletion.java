a18b0d05a783dbc55f22fc6d3b65be7533650f1b
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-06-21 21:54:51 -0500

    Create ShipmentCompletion.java

---
+++ src/main/java/com/example/tongue/integration/shipping/ShipmentCompletion.java
@@ -0,0 +1,15 @@
+package com.example.tongue.integration.shipping;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ShipmentCompletion {
+    private String artifactId;
+    private String shippingId;
+}
